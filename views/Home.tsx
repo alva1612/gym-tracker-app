e6914cb653b17025ce890313098df3fbd3186ded
Alvaro <alvaro.guillenc1612@gmail.com>
2023-09-07 23:22:41 -0500

    template tailwind on react native?

---
+++ views/Home.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { ScrollView, Text } from 'react-native'
+
+const Home = () => {
+  return (
+  <ScrollView>
+
+<Text>wow</Text>
+<Text>wow1</Text>
+<Text>wow2</Text>
+<Text>wow</Text>
+<Text>wow</Text>
+<Text>wow</Text>
+<Text>wow</Text>
+<Text>wow</Text>
+<Text>wow</Text>
+<Text>wow1</Text>
+<Text>wow2</Text>
+<Text>wow1</Text>
+<Text>wow2</Text>
+<Text>wow1</Text>
+<Text>wow2</Text>
+<Text>wow1</Text>
+<Text>wow2</Text>
+<Text>wow1</Text>
+<Text>wow2</Text>
+<Text>wow1</Text>
+<Text>wow2</Text>
+  </ScrollView>
+  )
+}
+
+export default Home
